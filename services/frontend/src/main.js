8bae89cf63ca429a5b7fd669b6b6d0dd5c085eef
David <itsdavid.otero@gmail.com>
2023-11-25 19:30:34 +0100

    Añadida configuración de axios al front

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -8,9 +8,19 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Axios
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5000/';
+
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(router).use(vuetify).mount('#app')
+createApp(App)
+	.use(router)
+	use(axios)
+	.use(vuetify)
+	.mount('#app')
